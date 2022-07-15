c4abd7930a9ff5f2b7ca267c0364aac25a26084b
David Gracia <david.gracia.celemendi@gmail.com>
2022-07-15 22:35:30 +0200

    change

--- arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/Result.kt
+++ arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/Result.kt
@@ -4,20 +4,27 @@ public sealed class Result<out A, out B> {
 
   //internal val either: Either<A,B> = TODO()
 
-  public inline fun <A, B, C> then(f: (B) -> Result<A, C>): Result<A, C> {
-    val either = when (this) {
-      is Failure -> Either.Left(this.value)
-      is Success -> Either.Right(this.value)
-    }
-
-    when(either) {
-      is Either.Left -> TODO()
-      is Either.Right -> TODO()
-    }
-  }
-
-
   public data class Success<out B> constructor(val value: B) : Result<Nothing, B>()
 
   public data class Failure<out A> constructor(val value: A) : Result<A, Nothing>()
+}
+
+public inline fun <A, B, C> Result<A, B>.then(f: (B) -> Result<A, C>): Result<A, C> {
+
+  val either = when (this) {
+    is Result.Failure -> Either.Left(this.value)
+    is Result.Success -> Either.Right(this.value)
+  }
+
+  val flatMap = either.flatMap { value: B ->
+    f(value).let { result: Result<A, C> -> when() }
+  }
+  when(flatMap) {
+
+  }
+
+  return when (this) {
+    is Result.Failure -> this
+    is Result.Success -> f(this.value)
+  }
 }
