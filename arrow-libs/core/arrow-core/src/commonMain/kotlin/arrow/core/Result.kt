9aa1c5e3495b7985109555b324300f7b8123f43a
David Gracia <david.gracia.celemendi@gmail.com>
2022-07-15 22:37:25 +0200

    change

--- arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/Result.kt
+++ arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/Result.kt
@@ -2,8 +2,6 @@ package arrow.core
 
 public sealed class Result<out A, out B> {
 
-  //internal val either: Either<A,B> = TODO()
-
   public data class Success<out B> constructor(val value: B) : Result<Nothing, B>()
 
   public data class Failure<out A> constructor(val value: A) : Result<A, Nothing>()
